72244cf96ef3197396b9c1c5deeb1a47dc2efe95
irem-ozturkcu <76103218+irem-ozturkcu@users.noreply.github.com>
2022-09-10 13:32:47 +0300

    Job Portal App

---
+++ JobPortalApp/app/src/main/java/com/jobportalapp/HomeActivity.java
@@ -0,0 +1,44 @@
+package com.jobportalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    private Button btnAllJob;
+    private Button btnPostJob;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btnAllJob=findViewById(R.id.btn_allJob);
+        btnPostJob=findViewById(R.id.btn_PostJob);
+
+        btnAllJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getApplicationContext(),AllJobActivity.class));
+
+            }
+        });
+
+
+        btnPostJob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getApplicationContext(),PostJobActivity.class));
+
+            }
+        });
+
+    }
+}
---
+++ JobPortalApp/app/src/main/java/com/jobportalapp/PostJobActivity.java
@@ -0,0 +1,14 @@
+package com.jobportalapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostJobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_job);
+    }
+}
